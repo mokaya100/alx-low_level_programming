f62f7ee8de73ca6f634561a27a6d6f1ac6ac931a
mokaya100 <mokayageoffrey844@gmail.com>
2024-02-08 08:55:44 -0800

    correction

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -6,11 +6,11 @@
  */
 int get_endianness(void)
 {
-  int num;
+	int num;
 
-  num = 1;
-  if (*(char *)&num == 1)
-    return (1);
-  else
-    return (0);
+	num = 1;
+	if (*(char *)&num == 1)
+		return (1);
+	else
+		return (0);
 }
