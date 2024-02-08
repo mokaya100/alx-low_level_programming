56775f067b4e830220602c5b5a67b7c84993e64d
mokaya100 <mokayageoffrey844@gmail.com>
2024-02-08 08:24:16 -0800

    correction

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -7,23 +7,23 @@
  */
 void print_binary(unsigned long int n)
 {
-  unsigned long int temp;
-  int shifts;
+	unsigned long int temp;
+	int shifts;
 
-  if (n == 0)
-    {
-      printf("0");
-      return;
-    }
+	if (n == 0)
+	{
+		printf("0");
+		return;
+	}
 
-  for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
-    ;
+	for (temp = n, shifts = 0; (temp >>= 1) > 0; shifts++)
+		;
 
-  for (; shifts >= 0; shifts--)
-    {
-      if ((n >> shifts) & 1)
-	printf("1");
-      else
-	printf("0");
-    }
+	for (; shifts >= 0; shifts--)
+	{
+		if ((n >> shifts) & 1)
+			printf("1");
+		else
+			printf("0");
+	}
 }
